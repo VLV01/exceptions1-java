895ed2f00630e2f2cba7a330e59d4ee776718137
VLV01 <vinicius.linsvasco@hotmail.com>
2022-01-20 16:30:05 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,26 +35,20 @@ public class Program {
 			System.out.println("Check-out date (dd/MM/yyy): ");
 			checkOut = sdf.parse(sc.next());
 			
-			Date now = new Date();
-			if (checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-			}
-			else if (!checkOut.after(checkIn)) {
-				System.out.println("Error in reservation: Reservation dates for update must be future dates");
-
-			}
-			else {
-				reservation.updateDates(checkIn, checkOut);
+	
+				String error = reservation.updateDates(checkIn, checkOut);
+				if (error != null ) {
+					System.out.println("Error in reservation: " + error);
+				}
+				else {
+					System.out.println("Reservation: " + reservation);
+				}
 				System.out.println("Reservation: " + reservation);
-			}
+			
 			
 			
 		}
 
-		
-		
-		
-		
 		
 		
 		sc.close();
